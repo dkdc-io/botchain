b11cd96b173a6b9808330e49b04a4c6cec0fa85a
Cody <cody@dkdc.dev>
2024-11-21 18:28:00 -0500

    initial commit

---
+++ src/botchain/strings.py
@@ -0,0 +1,7 @@
+bot_system_template = (
+    "You are a bot named {name} with an objective to {objective}.\n\n{instructions}"
+)
+
+bot_doc_template = "A bot named {name} with an objective to {objective}.\n\nThis bot will: {instructions}"
+
+tool_template = "    Tool name: {name}\n    Description: {description}"
